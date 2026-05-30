6c2bd0dae423f7da7bd2b6cb8888101ee9b6c3ab
Enebe <enebe.nb@gmail.com>
2026-05-30 08:34:33 -0300

    better handling of ProjectSettings defaults

--- addons/controller_icons/ControllerIconSettings.cs
+++ addons/controller_icons/ControllerIconSettings.cs
@@ -17,40 +17,47 @@ public static class ControllerIconSettings {
 		ProjectSettings.SetAsBasic(name, isBasic);
 	}
 
+	private const string defaultMapperFactory	= "res://addons/controller_icons/ControllerIconMapperFactory.cs";
+	private const string defaultJoypadFallback	= "Xbox 360 Controller";
+	private const float  defaultJoypadDeadzone	= 0.5f;
+	private const bool   defaultMouseRemap		= true;
+	private const int    defaultMouseMinMove	= 200;
+	private const string defaultConcatTexture	= "res://addons/controller_icons/assets/concat.png";
+
 	public static void PrepareAllSettings() {
-		PrepareSetting("controller_icons/general/mapper_factory", "res://addons/controller_icons/ControllerIconMapperFactory.cs", PropertyHint.File, "*.cs");
-		PrepareSetting("controller_icons/general/joypad_fallback", "Xbox 360 Controller");
-		PrepareSetting("controller_icons/general/joypad_deadzone", 0.5, PropertyHint.Range, "0.0,1.0");
-		PrepareSetting("controller_icons/general/allow_mouse_remap", true);
-		PrepareSetting("controller_icons/general/mouse_min_movement", 200, PropertyHint.Range, "0,10000");
-		PrepareSetting("controller_icons/general/custom_concat_texture", "res://addons/controller_icons/assets/concat.png", PropertyHint.File, "*.png");
+		PrepareSetting("controller_icons/general/mapper_factory", defaultMapperFactory, PropertyHint.File, "*.cs");
+		PrepareSetting("controller_icons/general/joypad_fallback", defaultJoypadFallback);
+		PrepareSetting("controller_icons/general/joypad_deadzone", defaultJoypadDeadzone, PropertyHint.Range, "0.0,1.0");
+		PrepareSetting("controller_icons/general/allow_mouse_remap", defaultMouseRemap);
+		PrepareSetting("controller_icons/general/mouse_min_movement", defaultMouseMinMove, PropertyHint.Range, "0,10000");
+		PrepareSetting("controller_icons/general/custom_concat_texture", defaultConcatTexture, PropertyHint.File, "*.png");
 	}
 
 	private static ControllerIconMapperFactory _mapperFactory;
 	// Custom mapper factory to find resource paths
 	public static ControllerIconMapperFactory MapperFactory {
 		get {
-			_mapperFactory ??= GD.Load<CSharpScript>(ProjectSettings.GetSettingWithOverride("controller_icons/general/mapper_factory").As<string>()).New().As<ControllerIconMapperFactory>();
+			_mapperFactory ??= GD.Load<CSharpScript>(ProjectSettings.GetSetting("controller_icons/general/mapper_factory", defaultMapperFactory).As<string>()).New().As<ControllerIconMapperFactory>();
 			return _mapperFactory;
 		}
 	}
 
 	// Controller type to fallback to if automatic
 	// controller detection fails
-	public static string JoypadFallback => ProjectSettings.GetSettingWithOverride("controller_icons/general/joypad_fallback").As<string>();
+	public static string JoypadFallback => ProjectSettings.GetSetting("controller_icons/general/joypad_fallback", defaultJoypadFallback).As<string>();
 
 	// Controller deadzone for triggering an icon remap when input
 	// is analogic (movement sticks or triggers)
-	public static float JoypadDeadzone => ProjectSettings.GetSettingWithOverride("controller_icons/general/joypad_deadzone").As<float>();
+	public static float JoypadDeadzone => ProjectSettings.GetSetting("controller_icons/general/joypad_deadzone", defaultJoypadDeadzone).As<float>();
 
 	// Allow mouse movement to trigger an icon remap
-	public static bool AllowMouseRemap => ProjectSettings.GetSettingWithOverride("controller_icons/general/allow_mouse_remap").As<bool>();
+	public static bool AllowMouseRemap => ProjectSettings.GetSetting("controller_icons/general/allow_mouse_remap", defaultMouseRemap).As<bool>();
 
 	// Minimum mouse "instantaneous" movement for
 	// triggering an icon remap
-	public static int MouseMinMovement => ProjectSettings.GetSettingWithOverride("controller_icons/general/mouse_min_movement").As<int>();
+	public static int MouseMinMovement => ProjectSettings.GetSetting("controller_icons/general/mouse_min_movement", defaultMouseMinMove).As<int>();
 
 	// Custom LabelSettings. If unset, uses engine default settings.
-	public static string CustomConcatTexture => ProjectSettings.GetSettingWithOverride("controller_icons/general/custom_concat_texture").As<string>();
+	public static string CustomConcatTexture => ProjectSettings.GetSetting("controller_icons/general/custom_concat_texture", defaultConcatTexture).As<string>();
 }
 
